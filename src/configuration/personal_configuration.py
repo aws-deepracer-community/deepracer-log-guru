38f8435b4b2c5ed2b7b0ae54f3b551d335697e37
David Mutton-Hughes <davidmuttonhughes@yahoo.co.uk>
2021-03-18 00:58:52 +0000

    Revert private config to standard (example) values for releases

--- src/configuration/personal_configuration.py
+++ src/configuration/personal_configuration.py
@@ -1,4 +1,4 @@
 
 # Replace this with the path of your own preferred directory that will contain your log files
 
-LOG_DIRECTORY = "C:\\Users\\david\\DeepRacer\\deep_racer_logs\\2021 PreSeason"
+LOG_DIRECTORY = "C:\\Users\\some\\where\\your_logs"

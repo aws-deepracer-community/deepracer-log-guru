447bb65ed68a440e8048d3b861ab50dd854b7b7b
David Mutton-Hughes <davidmuttonhughes@yahoo.co.uk>
2021-04-27 23:15:37 +0100

    Curve-fitting ... basic ability to re-align the angle of approach

--- src/analyze/track/analyze_curve_fitting.py
+++ src/analyze/track/analyze_curve_fitting.py
@@ -15,6 +15,7 @@ from src.graphics.track_graphics import TrackGraphics
 from src.sequences.sequence import Sequence
 from src.sequences.sequences import Sequences
 
+BACKWARDS_DISTANCE = 2
 
 class AnalyzeCurveFitting(TrackAnalyzer):
 
@@ -24,13 +25,14 @@ class AnalyzeCurveFitting(TrackAnalyzer):
 
         self._chosen_point = None
         self._chosen_bearing = None
+        self._backwards_point = None
 
     def build_control_frame(self, control_frame):
         pass
 
     def redraw(self):
         if self._chosen_point and self._chosen_bearing:
-            self.track_graphics.plot_angle_line(self._chosen_point, self._chosen_bearing - 180, 2, 2, "green")
+            self.track_graphics.plot_angle_line(self._chosen_point, self._chosen_bearing - 180, BACKWARDS_DISTANCE, 2, "green")
             for s in self._all_sequences.get_all():
                 # for s in self._all_sequences.get_matches(None, None, None, (-20, -30)):
                 points = s.get_plot_points(self._chosen_point, self._chosen_bearing)
@@ -38,19 +40,30 @@ class AnalyzeCurveFitting(TrackAnalyzer):
                     self.track_graphics.plot_dot(p, 2, "green")
 
     def right_button_pressed(self, chosen_point):
-        waypoint_id = self.current_track.get_closest_waypoint_id(chosen_point)
-        waypoint = self.current_track.get_waypoint(waypoint_id)
+        chose_backwards_point = False
+        if self._backwards_point:
+            backwards_distance = geometry.get_distance_between_points(chosen_point, self._backwards_point)
+            primary_distance = geometry.get_distance_between_points(chosen_point, self._chosen_point)
+            chose_backwards_point = backwards_distance < 0.5 * primary_distance and abs(primary_distance - BACKWARDS_DISTANCE) < 0.25 * BACKWARDS_DISTANCE
 
-        distance_from_waypoint = geometry.get_distance_between_points(waypoint, chosen_point)
-        max_distance_from_centre = (self.current_track.get_width() + VEHICLE_WIDTH) / 2
-
-        if distance_from_waypoint > max_distance_from_centre:
-            bearing_of_point = geometry.get_bearing_between_points(waypoint, chosen_point)
-            self._chosen_point = geometry.get_point_at_bearing(waypoint, bearing_of_point, max_distance_from_centre)
+        if chose_backwards_point:
+            self._backwards_point = chosen_point
+            self._chosen_bearing = geometry.get_bearing_between_points(self._backwards_point, self._chosen_point)
         else:
-            self._chosen_point = chosen_point
+            waypoint_id = self.current_track.get_closest_waypoint_id(chosen_point)
+            waypoint = self.current_track.get_waypoint(waypoint_id)
 
-        self._chosen_bearing = self.current_track.get_bearing_at_waypoint(waypoint_id)
+            distance_from_waypoint = geometry.get_distance_between_points(waypoint, chosen_point)
+            max_distance_from_centre = (self.current_track.get_width() + VEHICLE_WIDTH) / 2
+
+            if distance_from_waypoint > max_distance_from_centre:
+                bearing_of_point = geometry.get_bearing_between_points(waypoint, chosen_point)
+                self._chosen_point = geometry.get_point_at_bearing(waypoint, bearing_of_point, max_distance_from_centre)
+            else:
+                self._chosen_point = chosen_point
+
+            self._chosen_bearing = self.current_track.get_bearing_at_waypoint(waypoint_id)
+            self._backwards_point = geometry.get_point_at_bearing(self._chosen_point, self._chosen_bearing - 180, BACKWARDS_DISTANCE)
 
         self.guru_parent_redraw()
 

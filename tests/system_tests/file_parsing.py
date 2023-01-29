e0db075d3b516901ceab31eeedc4629953ae0323
david <davidmuttonhughes@yahoo.co.uk>
2023-01-29 18:54:06 +0000

    #184 - Move test input files into a sub-directory

--- tests/system_tests/file_parsing.py
+++ tests/system_tests/file_parsing.py
@@ -7,6 +7,7 @@ from src.ui.please_wait import PleaseWait
 from src.log.log import Log
 
 RESOURCE_DIR = os.path.join("resources", "file_parsing")
+INPUT_FILES_DIR = os.path.join(RESOURCE_DIR, "input_log_files")
 EXPECTED_RESULT_DIR = os.path.join(RESOURCE_DIR, "expected_output")
 
 FILE_EXTENSION = ".meta.json"
@@ -64,10 +65,11 @@ class TestFileParsingWithJsonOutput(unittest.TestCase):
     def _test_parse_file(self, filename: str):
         # Setup
         self.assertTrue(os.path.isdir(RESOURCE_DIR))
+        self.assertTrue(os.path.isdir(INPUT_FILES_DIR))
         self.assertTrue(os.path.isdir(EXPECTED_RESULT_DIR))
 
-        input_file = os.path.join(RESOURCE_DIR, filename)
-        actual_output_file = os.path.join(RESOURCE_DIR, filename + FILE_EXTENSION)
+        input_file = os.path.join(INPUT_FILES_DIR, filename)
+        actual_output_file = os.path.join(INPUT_FILES_DIR, filename + FILE_EXTENSION)
         expected_output_file = os.path.join(EXPECTED_RESULT_DIR, filename + FILE_EXTENSION)
 
         self.assertTrue(os.path.isfile(input_file))
@@ -78,7 +80,7 @@ class TestFileParsingWithJsonOutput(unittest.TestCase):
 
         # Execute
         please_wait = DummyPleaseWait()
-        log = Log(RESOURCE_DIR)
+        log = Log(INPUT_FILES_DIR)
         log.parse(filename, please_wait, 10, 20)
         log.save()
 

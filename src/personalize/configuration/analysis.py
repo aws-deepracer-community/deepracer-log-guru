d0d4d09ca041c26b20cc0f35918cdd95cca04617
david <davidmuttonhughes@yahoo.co.uk>
2023-09-09 14:33:39 +0100

    Latest competition tracks - Release 3.2.26

--- src/personalize/configuration/analysis.py
+++ src/personalize/configuration/analysis.py
@@ -6,7 +6,7 @@
 # Copyright (c) 2021 dmh23
 #
 
-from src.personalize.reward_functions.follow_centre_line import reward_function
+from src.personalize.reward_functions.private.sep_2023_version_g import reward_function
 
 NEW_REWARD_FUNCTION = reward_function
 
